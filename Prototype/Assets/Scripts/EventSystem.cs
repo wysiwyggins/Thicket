1c04b9d2dbd4d43a60175513704e036a08142095
Wiley Wiggins <weevil@wileywiggins.com>
2020-04-04 23:36:15 -0500

    added some gamestates

---
+++ Prototype/Assets/Scripts/EventSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { NightTime, PlayerMove, AITurn, GameOver};
+
+public class EventSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
