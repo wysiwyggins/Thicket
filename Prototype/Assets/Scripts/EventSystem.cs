ffe4a9305d70e50dda484ea090efba84adb0373b
Wiley Wiggins <weevil@wileywiggins.com>
2020-04-06 23:03:40 -0500

    changed conditionals to switch cases in eventsystem

--- Prototype/Assets/Scripts/EventSystem.cs
+++ Prototype/Assets/Scripts/EventSystem.cs
@@ -10,6 +10,7 @@ public class EventSystem : MonoBehaviour
 
     public GameState CurrentState;
     public int players = 1;
+    public string debugStateText;
 
     // Start is called before the first frame update
     void Start()
@@ -20,33 +21,38 @@ public class EventSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (CurrentState == GameState.Attract)
-        {
-            //listen for the number of players and if player doesn't equal zero change state to PlayerMove
-        }
 
-        if (CurrentState == GameState.PlayerTurn)
-        {
+        switch (CurrentState)
+        { 
 
-            //if no pieces have isPlayer switch CurrentState to GameOver
+            case GameState.Attract:
+                debugStateText = "Gamestate: Attract";
+                //listen for the number of players and if player doesn't equal zero change state to PlayerMove
+                break;
+                
 
-            //for pieces that have isPlayer set to true: highlight tiles (that aren't obstacles) 1 hex adjacent to the piece
-            //if the player clicks on one, move() the player character to that hex
-            //when done, switch CurrentState to AITurn
-        }
+            case GameState.PlayerTurn:
+                debugStateText = "Gamestate: PlayerTurn";
+                //if no pieces have isPlayer switch CurrentState to GameOver
+                //for pieces that have isPlayer set to true: highlight tiles (that aren't obstacles)
+                //1 hex adjacent to the piece
+                //if the player clicks on one, move() the player character to that hex
+                //when done, switch CurrentState to AITurn
+                break;
 
-        if (CurrentState == GameState.AITurn)
-        {
-            // for pieces that have isPlayer set to false:
-            // speed of the piece is its range- if the pieces prey or the scent of the pray is inside the range, move to it
-            // if no prey or scent of prey is in range: if a prey piece is on any tile on the board that is not isDark, move towards it
-            // when done, switch CurrentState to PlayerTurn
-        }
+            case GameState.AITurn:
+                debugStateText = "Gamestate: AITurn";
+                // for pieces that have isPlayer set to false:
+                // speed of the piece is its range- if the pieces prey or the scent of the pray is inside the range, move to it
+                // if no prey or scent of prey is in range: if a prey piece is on any tile on the board that is not isDark, move towards it
+                // when done, switch CurrentState to PlayerTurn
+                break;
 
-        if (CurrentState == GameState.GameOver)
-        {
-            // play the sad song
-            // set state to attract
+            case GameState.GameOver:
+                debugStateText = "Gamestate: Gameover";
+                // play the sad song
+                // set state to attract
+                break;
         }
     }
 }
