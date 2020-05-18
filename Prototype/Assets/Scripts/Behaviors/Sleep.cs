233a4080f18ae1db131a9b0814e722249220ba5e
Wiley Wiggins <weevil@wileywiggins.com>
2020-05-18 15:43:08 -0500

    spent about 4 hours trying to make a scrollbar that previously worked work again, still broken.

---
+++ Prototype/Assets/Scripts/Behaviors/Sleep.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Sleep : PieceBehaviour
+{
+
+	// add this script for pieces that sleep
+    // isNocturnal determines if the piece sleeps at night or day
+    // sleep sprite displays when asleep
+    // home tile is the tile the piece tries to return to during dawn or dusk depending on isNocturnal
+    // we need to get the time of day from PieceManager
+
+
+	public bool isNocturnal;
+	public Tilemap Terrain;
+	public Tile HomeTile;
+	public GameObject SleepSprite;
+
+	public override void Begin()
+	{
+
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+
+	}
+
+	private void Update()
+	{
+
+	}
+
+}
