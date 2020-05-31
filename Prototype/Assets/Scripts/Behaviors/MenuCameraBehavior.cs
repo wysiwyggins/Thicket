c399ff7619c234beeab6252c7697502db1741546
Wiley Wiggins <weevil@wileywiggins.com>
2020-05-31 01:02:31 -0500

    added attract scene

---
+++ Prototype/Assets/Scripts/Behaviors/MenuCameraBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraBehavior : MonoBehaviour
+{
+
+	public Color color1 = Color.red;
+	public Color color2 = Color.blue;
+	public float duration = 20.0F;
+
+	Camera cam;
+
+	// Use this for initialization
+	void Start()
+	{
+		cam = GetComponent<Camera>();
+		cam.clearFlags = CameraClearFlags.SolidColor;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float t = Mathf.PingPong(Time.time, duration) / duration;
+		cam.backgroundColor = Color.Lerp(color1, color2, t);
+	}
+}
