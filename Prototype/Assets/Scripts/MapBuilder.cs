65793a95285bbbfa9b1907a93f5842a8008d2046
Wiley Wiggins <weevil@wileywiggins.com>
2020-09-21 13:30:04 -0700

    comment cleanup

--- Prototype/Assets/Scripts/MapBuilder.cs
+++ Prototype/Assets/Scripts/MapBuilder.cs
@@ -29,8 +29,6 @@ public class MapBuilder : MonoBehaviour
     // lays down floor tiles based on size var
     // randomly creates a maze of scenery and obstacle tiles
     // I'd like to use this- http://gamelogic.co.za/grids/features/examples-that-ship-with-grids/prims-maze-generation-algorithm-on-a-hexagonal-grid/
-    // right now the obstacle bool of scenery doesn't actually do anything, and obstacle tiles are painted manually,
-    // but something like this might work- pf.SetNavTileBlocked(grid.WorldToCell(transform.position), true);
     // randomly place water and animals and plants based on public variables for each
 
 
